52d8ba0b8e32a2a2ef6fa2067b580c5c19bb8968
nsshah1986 <nsshah_1986@yahoo.co.in>
2023-03-21 06:44:21 +0530

    Pyest folder and basic code added

---
+++ Pytest_topic/py_assertions/test_module_001.py
@@ -0,0 +1,15 @@
+def test_a1():
+    assert 4!=3
+
+def test_a2():
+    assert 1
+
+def test_a3():
+    assert "abc" == "abcd"
+
+def test_a4():
+    assert ((43-1)*4/2)==4
+
+def test_a5():
+    assert 1 in divmod(9,5)
+    assert "py" in " this is pytest"
---
+++ Pytest_topic/test_module01.py
@@ -0,0 +1,14 @@
+
+def test_a1():
+    assert 5+5==10
+    assert 5-5==0
+    assert 5*5==25
+    assert 5/5==1
+
+# this is failed test
+
+def test_a2():
+    assert 9/5==1.5
+
+def test_a3():
+    assert 9//5==1
